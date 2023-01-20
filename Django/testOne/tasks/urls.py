75bbdd4ee326c658e740a303ecf564f769b623db
sunnyallana <study.sunnyallana@gmail.com>
2023-01-21 03:02:53 +0500

    changes

--- Django/testOne/tasks/urls.py
+++ Django/testOne/tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpattern = [
-    path('', views.tasks, name="index"),
+    path("", views.index, name="index"),
 ]
--- Django/testOne/tasks/views.py
+++ Django/testOne/tasks/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def tasks(request):
-    
+def index(request):
+
