7153a0196e6318ee83681f88757b0399d1ecf817
sunnyallana <study.sunnyallana@gmail.com>
2023-01-22 03:22:40 +0500

    DjangoFormsSupport

--- Django/testOne/tasks/views.py
+++ Django/testOne/tasks/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from django import forms
 # Create your views here.
 tasksList = ["Eat", "Code", "Sleep", "Repeat"]
+class addTask(forms.Form):
+    task = forms.CharField(label = "New Task")
 def index(request):
     return render(request, "tasks/index.html", {"tasksList": tasksList})
 def add(request):
-    return render(request, "tasks/add.html")
+    return render(request, "tasks/add.html", {"addTask": addTask()})
