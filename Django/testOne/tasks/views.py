54c898d3536b1a8250eda7e1e57852b5a3453d79
sunnyallana <study.sunnyallana@gmail.com>
2023-01-22 05:49:27 +0500

    FormSubmission

--- Django/testOne/tasks/views.py
+++ Django/testOne/tasks/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django import forms
 # Create your views here.
-tasksList = ["Eat", "Code", "Sleep", "Repeat"]
+tasksList = ["Eat","Sleep"]
 class addTask(forms.Form):
     task = forms.CharField(label = "New Task")
+
 def index(request):
     return render(request, "tasks/index.html", {"tasksList": tasksList})
+
 def add(request):
+    if request.method == "POST":
+        form = addTask(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasksList.append(task)
+        else:
+            return render(request, "tasks/add.html", {"addTask": form})
     return render(request, "tasks/add.html", {"addTask": addTask()})
