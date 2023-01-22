450d1552dbf260cb6bad696baec5b9b54feb4a64
sunnyallana <study.sunnyallana@gmail.com>
2023-01-22 06:25:14 +0500

    Server-side validation

--- Django/testOne/tasks/views.py
+++ Django/testOne/tasks/views.py
@@ -3,17 +3,17 @@ from django import forms
 # Create your views here.
 tasksList = ["Eat","Sleep"]
 class addTask(forms.Form):
-    task = forms.CharField(label = "New Task")
+    taskGet = forms.CharField(label = "New Task")
 
 def index(request):
     return render(request, "tasks/index.html", {"tasksList": tasksList})
 
 def add(request):
     if request.method == "POST":
-        form = addTask(request.POST)
-        if form.is_valid():
-            task = form.cleaned_data["task"]
-            tasksList.append(task)
+        formWithUserInput = addTask(request.POST)
+        if formWithUserInput.is_valid():
+            taskStore = formWithUserInput.cleaned_data["taskGet"]
+            tasksList.append(taskStore)
         else:
-            return render(request, "tasks/add.html", {"addTask": form})
-    return render(request, "tasks/add.html", {"addTask": addTask()})
+            return render(request, "tasks/add.html", {"formPlug": formWithUserInput})
+    return render(request, "tasks/add.html", {"formPlug": addTask()})
