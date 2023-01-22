0bcf23ca86046bed7f364b65705a1c59f635a68e
sunnyallana <study.sunnyallana@gmail.com>
2023-01-23 00:10:20 +0500

    Migration

--- Django/testOne/tasks/views.py
+++ Django/testOne/tasks/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from django import forms
 # Create your views here.
-tasksList = ["Eat","Sleep"]
 class addTask(forms.Form):
     taskGet = forms.CharField(label = "New Task")
 
 def index(request):
-    return render(request, "tasks/index.html", {"tasksList": tasksList})
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html", {"tasksList": request.session["tasks"]})
 
 def add(request):
     if request.method == "POST":
         formWithUserInput = addTask(request.POST)
         if formWithUserInput.is_valid():
             taskStore = formWithUserInput.cleaned_data["taskGet"]
-            tasksList.append(taskStore)
+            request.session["tasks"] += taskStore
         else:
             return render(request, "tasks/add.html", {"formPlug": formWithUserInput})
     return render(request, "tasks/add.html", {"formPlug": addTask()})
