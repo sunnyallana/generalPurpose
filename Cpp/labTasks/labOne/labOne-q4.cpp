7bb390506d6eaf3bcd0881d6d7c2d42558fdbfa4
sunnyallana <study.sunnyallana@gmail.com>
2023-01-24 21:18:56 +0500

    tasks

---
+++ Cpp/labTasks/labOne/labOne-q4.cpp
@@ -0,0 +1,32 @@
+// Preprocessing Directives
+#include <iostream>
+using namespace std;
+// Defining Main
+int main(void){
+// Declaring necessary variables
+	int array[100];
+	int size;
+	int found = 0;
+// Taking input from user
+	cout << "Enter the size of integer array: ";
+	cin >> size;
+	for(int i = 0; i < size; i++){
+			cout << "Enter element: ";
+			cin >> array[i];
+		}
+// Computing and displaying the first repeating element
+	for(int i = 0; i < size && found == 0; i++){
+		for(int j = i+1; j < size && found == 0; j++){
+			if(array[i] == array[j]){
+				cout << array[i] << " is the first repeating element";
+				found = 1;
+			}
+		}
+	}
+// To display a message if no repeating elements are found
+	if( found == 0){
+		cout << "No repeating elements found";
+	}
+// Returning control to OS
+return 0;
+}
